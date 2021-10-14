eba113f3367a056cad1598528aa0e167715fe925
Quasi <quasipersonal58@gmail.com>
2021-10-14 13:08:54 +0800

    Add transition animation

--- src/app/renderer/login/components/selectLogin/SelectLogin.tsx
+++ src/app/renderer/login/components/selectLogin/SelectLogin.tsx
@@ -1,26 +1,42 @@
 import React from "react";
 import styles from "./SelectLogin.scss";
+import Trail from "../../../common/animations/components/trail/Trail";
 
 import { useTranslation } from "react-i18next";
-export default function SelectLogin() {
+
+type IProps = {
+    onMojangClick: () => void;
+}
+
+export default function SelectLogin(props: IProps) {
 
     const { t } = useTranslation();
 
+    const [open, setOpen] = React.useState(true);
+
     return (
         <div className={styles.selectLoginDiv}>
-            <h1>{t("login.title")}</h1>
-            <h2>{t("login.subTitle_1")}</h2>
-            <div className={styles.buttonDiv}>
-                <button>
-                    <p>Microsoft</p>
-                    <span>登入</span>
-                </button>
-                <br />
-                <button>
-                    <p>Mojang</p>
-                    <span>登入</span>
-                </button>
-            </div>
+
+            <Trail open={open}>
+                <h1>{t("login.title")}</h1>
+                <h2>{t("login.subTitle_1")}</h2>
+                <div className={styles.buttonDiv}>
+                    <button>
+                        <p>Microsoft</p>
+                        <span>登入</span>
+                    </button>
+                    <br />
+                    <button onClick={() => {
+                        
+                        setOpen(false);
+                        setTimeout(props.onMojangClick, 200);
+
+                    }}>
+                        <p>Mojang</p>
+                        <span>登入</span>
+                    </button>
+                </div>
+            </Trail>
         </div>
     );
 }
