6b690281fd3777b3a0c637ddb017b71bf7ee5a2a
Quasi <quasipersonal58@gmail.com>
2021-10-14 13:12:44 +0800

    Add auto toggle component code

--- src/app/renderer/login/Login.tsx
+++ src/app/renderer/login/Login.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import styles from "./Login.scss";
 
+import SelectLogin from "./components/selectLogin/SelectLogin";
+import MojangLogin from "./components/mojangLogin/mojangLogin";
 export default function Login() {
+
+    const [toggleDiv, setToggleDiv] = React.useState(true);
+
     return (
-        <div>
-            Login
+        <div className={styles.LoginDiv}>
+            {
+                toggleDiv ? <SelectLogin onMojangClick={() => setToggleDiv(false)} /> : <MojangLogin onBackClick={() => setToggleDiv(true)} />
+            }
         </div>
     );
 }
