d9c82b7fc61d5c0b604026b8ad2da68073c19f2c
Quasi <quasipersonal58@gmail.com>
2021-10-14 17:42:57 +0800

    Updata code

--- src/app/renderer/login/Login.tsx
+++ src/app/renderer/login/Login.tsx
@@ -3,15 +3,42 @@ import styles from "./Login.scss";
 
 import SelectLogin from "./components/selectLogin/SelectLogin";
 import MojangLogin from "./components/mojangLogin/mojangLogin";
+import LoginLoading from "./components/loginLoading/LoginLoading";
 export default function Login() {
 
     const [toggleDiv, setToggleDiv] = React.useState(true);
+    const [loading, setLoading] = React.useState(false);
 
     return (
         <div className={styles.LoginDiv}>
             {
-                toggleDiv ? <SelectLogin onMojangClick={() => setToggleDiv(false)} /> : <MojangLogin onBackClick={() => setToggleDiv(true)} />
+                loading
+                ?
+                <LoginLoading />
+                :
+                toggleDiv
+                ? 
+                <SelectLogin onMicrosoftClick={microsoftLogin} onMojangClick={() => setToggleDiv(false)} />
+                : 
+                <MojangLogin onBackClick={() => setToggleDiv(true)} onLoginClick={() => {
+
+                    setLoading(true);
+                    minecraftLogin();
+
+                }} />
             }
         </div>
     );
+}
+
+function microsoftLogin(): void {
+
+    console.log("microsoft login click!");
+
+}
+
+function minecraftLogin(): void {
+
+    console.log("minecraft login click!");
+
 }
