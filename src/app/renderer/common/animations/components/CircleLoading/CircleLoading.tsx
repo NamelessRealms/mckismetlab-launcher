db52e0c0032be64629813a69e9c6d6e579b72d79
Quasi <quasipersonal58@gmail.com>
2021-10-15 09:40:12 +0800

    Add CircleLoading component

---
+++ src/app/renderer/common/animations/components/CircleLoading/CircleLoading.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import styles from "./CircleLoading.scss";
+
+import { animated, useTrail } from "react-spring";
+export default function CircLoading() {
+
+    const [open, setOpen] = React.useState(true);
+
+    const trail = useTrail(3, {
+        config: {
+            mass: 5,
+            tension: 2000,
+            friction: 200
+        },
+        opacity: open ? 1 : 0,
+        y: open ? 0 : 30,
+        from: {
+            opacity: 0,
+            y: 30
+        }
+    });
+
+    setTimeout(() => setOpen((value) => !value), 500);
+
+    return (
+        <div className={styles.circleLoadingDiv}>
+            {
+                trail.map(({ ...style }, index) => (
+                    <animated.div
+                        key={index}
+                        style={style}
+
+                    >
+                        <div className={styles.circle}></div>
+                    </animated.div>
+                ))
+            }
+        </div>
+    );
+}
