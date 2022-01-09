ea87fb9e9775ac0d8b01eb62dfbb6b64ae802af3
Quasi <quasipersonal58@gmail.com>
2022-01-09 16:11:22 +0800

    Add Checkbox component.

---
+++ src/app/renderer/common/components/checkbox/Checkbox.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "./Checkbox.scss";
+
+type IProps = {
+    checked: boolean;
+    content?: string;
+    className?: string;
+    onClickChecked?: (state: boolean) => void;
+}
+export default function Checkbox(props: IProps) {
+
+    return (
+        <div className={`${styles.checkboxDiv} ${props.className}`}>
+            
+            <label className={styles.container}>{props.content}
+                <input type="checkbox" className={styles.checkbox} checked={props.checked} onChange={(event) => {
+
+                    if(props.onClickChecked === undefined) return;
+                    props.onClickChecked(event.target.checked);
+
+                }} />
+                <span className={styles.checkMark}></span>
+            </label>
+
+        </div>
+    );
+}
