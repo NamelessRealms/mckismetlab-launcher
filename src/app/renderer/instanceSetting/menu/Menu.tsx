09dcb43be3deb671af1015f9710f4241f7572b6f
Quasi <quasipersonal58@gmail.com>
2022-01-09 16:18:06 +0800

    Create instanceSetting component menu component.

---
+++ src/app/renderer/instanceSetting/menu/Menu.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import ButtonFocus from "../../common/components/buttonFocus/ButtonFocus";
+import styles from "./Menu.scss";
+
+type IProps = {
+    menuType: number;
+    onClickMenuButton?: (menuType: number) => void;
+}
+
+export default function menu(props: IProps) {
+
+    const [menuType, setMenuType] = React.useState(props.menuType);
+
+    const onClickMenuButton = (type: number) => {
+
+        setMenuType(type);
+
+        if(props.onClickMenuButton !== undefined) {
+            props.onClickMenuButton(type);
+        }
+    }
+
+    return (
+        <div className={styles.menuDiv}>
+
+            <div className={styles.menuContentDiv}>
+
+                <div className={styles.menuContentButtonDiv}>
+
+                    <h1>設定</h1>
+                    <div>
+                        <h2
+                            style={menuType === 1 ? { background: "#1E1E1E", color: "#ffff" } : {}}
+                            onClick={() => onClickMenuButton(1)}
+                        >Java</h2>
+                        <h2
+                            style={menuType === 2 ? { background: "#1E1E1E", color: "#ffff" } : {}}
+                            onClick={() => onClickMenuButton(2)}
+                        >模組</h2>
+                        <h2
+                            style={menuType === 3 ? { background: "#1E1E1E", color: "#ffff" } : {}}
+                            onClick={() => onClickMenuButton(3)}
+                        >資源包</h2>
+                    </div>
+
+                    <div className={styles.tr}></div>
+
+                    <h1>資料</h1>
+                    <div>
+                        <h2
+                            style={menuType === 4 ? { background: "#1E1E1E", color: "#ffff" } : {}}
+                            onClick={() => onClickMenuButton(4)}
+                        >螢幕截圖</h2>
+                    </div>
+
+                    <div className={styles.tr}></div>
+
+                    <h1>功能</h1>
+                    <div className={styles.functionButtonDiv}>
+
+                        <ButtonFocus className={styles.functionButton} content="目錄資料夾" />
+                        <ButtonFocus className={styles.functionButton} content="模組資料夾" />
+                        <ButtonFocus className={`${styles.functionButton} ${styles.functionFixButton}`} content="掃描與修復" />
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    );
+}
