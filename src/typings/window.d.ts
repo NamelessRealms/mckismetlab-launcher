498839130c2930bd00c464d106940434b91074e1
Quasi <quasipersonal58@gmail.com>
2022-01-11 20:50:34 +0800

    Add interface.

--- src/typings/window.d.ts
+++ src/typings/window.d.ts
@@ -1,3 +1,18 @@
 declare interface Window {
-    electron: any
+    electron: electron
+}
+
+interface electron {
+    windowApi: {
+        minimize: () => void,
+        maximize: () => void,
+        close: () => void,
+    },
+    uuid: {
+        getUUIDv4: () => string,
+    },
+    io: {
+        getRamSizeMax: () => Map<any, any>,
+    }
+
 }
