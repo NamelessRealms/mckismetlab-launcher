49506ea61c8bc43f18c76be15830d76e50ec42e5
Quasi <quasipersonal58@gmail.com>
2022-01-16 22:11:26 +0800

    Create Config class.

---
+++ src/main/config/Config.ts
@@ -0,0 +1,20 @@
+export default class Config {
+
+    public static get updateServerUrl(): string {
+        return "http://mckismetlab.net:56100";
+    }
+
+    public static get apiUrl(): string {
+        // return "http://220.134.105.30:8030";
+        return "http://220.134.105.30:8030";
+    }
+
+    public static get launcherAssetsUrl(): string {
+        return "http://220.134.105.30:8030/launcher/assets";
+    }
+
+    public static get serverPageUrl(): string {
+        return "http://220.134.105.30:8030/launcher/page";
+    }
+
+}
