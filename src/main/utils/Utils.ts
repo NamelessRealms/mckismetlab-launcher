0771142f3486a59a398cf75920c62eb378b09b08
Quasi <quasipersonal58@gmail.com>
2022-01-17 15:31:03 +0800

    Add isVersion method.

--- src/main/utils/Utils.ts
+++ src/main/utils/Utils.ts
@@ -42,4 +42,30 @@ export default class Utils {
     public static urlLastName(url: string): string | undefined {
         return url.split("/").pop();
     }
+
+    public static isVersion(currVer: string, promoteVer: string): boolean {
+
+        currVer = currVer || "0.0.0";
+        promoteVer = promoteVer || "0.0.0";
+
+        if (currVer === promoteVer) return false;
+
+        let currVerArr = currVer.split(".");
+        let promoteVerArr = promoteVer.split(".");
+
+        let len = Math.max(currVerArr.length, promoteVerArr.length);
+
+        for (let i = 0; i < len; i++) {
+            let proVal = ~~promoteVerArr[i];
+            let curVal = ~~currVerArr[i];
+
+            if (proVal < curVal) {
+                return false;
+            } else if (proVal > curVal) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
