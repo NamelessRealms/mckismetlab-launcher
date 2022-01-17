90c02e13a119254f6bb3af592fd5ee357781ee8e
Quasi <quasipersonal58@gmail.com>
2022-01-17 15:36:45 +0800

    Create ForgeVersionJsonParser class.

---
+++ src/main/parser/ForgeVersionJsonParser.ts
@@ -0,0 +1,29 @@
+import Utils from "../utils/Utils";
+import IForgeVersionLibraries from "../../interfaces/IForgeVersionLibraries";
+
+export default class ForgeVersionJsonParser {
+    
+    private _minecraftVersion;
+    private _forgeVersionJsonData;
+
+    constructor(forgeVersionJsonData: any, minecraftVersion: string) {
+        this._minecraftVersion = minecraftVersion;
+        this._forgeVersionJsonData = forgeVersionJsonData;
+    }
+
+    public get libraries(): Array<IForgeVersionLibraries> {
+        return this._forgeVersionJsonData.libraries;
+    }
+
+    public get mainClass(): string {
+        return this._forgeVersionJsonData.mainClass;
+    }
+
+    public get minecraftArguments(): any {
+        if (Utils.isMcVersion("1.13", this._minecraftVersion)) {
+            return this._forgeVersionJsonData.arguments;
+        } else {
+            return this._forgeVersionJsonData.minecraftArguments;
+        }
+    }
+}
